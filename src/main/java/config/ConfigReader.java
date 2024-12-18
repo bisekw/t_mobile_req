9a77eaaa49a392e297d7badf4deca1da76c77cb1
wbisek <bisekw@gmail.com>
2024-12-18 23:03:07 +0100

    GridRemote

--- src/main/java/config/ConfigReader.java
+++ src/main/java/config/ConfigReader.java
@@ -23,6 +23,7 @@ public class ConfigReader {
         log.info("Wycztałem konfigurację z pliku: "+configPath);
         InputStream inputStream = new FileInputStream(configPath);
         p.load(inputStream);
+        Settings.GRID_URL = p.getProperty("GRID_URL");
         Settings.CHROME_VERSION = p.getProperty("CHROME_VERSION");
         Settings.TESTED_APP_URL = p.getProperty("TESTED_APP_URL");
         Settings.RP_ENDPOINT = p.getProperty("RP_ENDPOINT");
